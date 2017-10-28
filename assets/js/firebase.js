36534a057d3bc16ca27c3eadfde49be6fd59ab0b
Sylvrleef <sylvrleef@gmail.com>
2017-10-28 10:12:58 -0500

    Initial set up

---
+++ assets/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCX9ehwQ9kNkKOGKJP_HGSawhb7PXA8lRs",
+    authDomain: "trainschedule-3e636.firebaseapp.com",
+    databaseURL: "https://trainschedule-3e636.firebaseio.com",
+    projectId: "trainschedule-3e636",
+    storageBucket: "",
+    messagingSenderId: "482500583053"
+  };
+  firebase.initializeApp(config);
